6fc6a24df554ff3c2a4013eef35ebf9d1f5b963e
blueye0704 <blueye0704@naver.com>
2024-06-21 17:44:42 +0900

    login 필터 구현 userDetail 및 service 커스텀 구현 spring security 설정 변경

---
+++ src/main/java/com/example/springjwttest/controller/JoinController.java
@@ -0,0 +1,26 @@
+package com.example.springjwttest.controller;
+
+import com.example.springjwttest.dto.JoinDTO;
+import com.example.springjwttest.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        System.out.println(joinDTO.getUsername());
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+
+}
---
+++ src/main/java/com/example/springjwttest/service/JoinService.java
@@ -0,0 +1,39 @@
+package com.example.springjwttest.service;
+
+import com.example.springjwttest.dto.JoinDTO;
+import com.example.springjwttest.entity.UserEntity;
+import com.example.springjwttest.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JoinService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if (isExist) {
+            System.out.println("존재한다 이미");
+            return;
+        }
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+    }
+
+}
+
